296d99f2566f7041b103f03012a1c6b13c164b35
Dan Treasure <dtreasure@getjive.com>
2014-04-04 14:33:23 -0600

    Nested views for goals view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,4 +26,22 @@ angular.module('yardstickApp')
 			url: "/goals",
 			templateUrl: "../views/goals.html"
 		})
+			.state('goals.all', {
+				templateUrl: '../views/goals.all.html'
+			})
+			.state('goals.physical', {
+				templateUrl: '../views/goals.physical.html'
+			})
+			.state('goals.spiritual', {
+				templateUrl: '../views/goals.spiritual.html'
+			})
+			.state('goals.professional', {
+				templateUrl: '../views/goals.professional.html'
+			})
+			.state('goals.financial', {
+				templateUrl: '../views/goals.financial.html'
+			})
+			.state('goals.other', {
+				templateUrl: '../views/goals.other.html'
+			})
 });
